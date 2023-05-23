8231923c665f1f30ff1e571667aa48582b2e0d52
yryo1005 <33744568+yryo1005@users.noreply.github.com>
2023-05-23 16:30:49 +0900

    Update Indian.py

--- Indian.py
+++ Indian.py
@@ -1,52 +1,48 @@
 from tensorflow.keras import optimizers
 
 class Indian(optimizers.Optimizer):
-    """
-        iterative fomula of Indian is 
-            w = w + lr * ( (1/beta - alpha) * w - 1/beta * z - beta * g )
-            z = z + lr * ( (1/beta - alpha) * w - 1/beta * z )
-            
-        where, w : parameter, g : gradient, 
-               lr : learning rate, aplha : hyper parameter, beta : hyper parameter
-    """
-    def __init__(self, learning_rate = 0.001, alpha = 1.0, beta = 1.5, name = "Indian"):
+    def __init__(self, learning_rate = 0.01, alpha = 0.5, beta = 0.1, name = "Indian"):
         super().__init__(name = name)
-
+        # 学習率の設定には _build_learning_rate 関数を用いる
         self._learning_rate = self._build_learning_rate(learning_rate)
         self._alpha = alpha
         self._beta = beta
     
+    # build 関数は保持する変数を定義する関数
     def build(self, var_list):
-        ###
+        
         super().build(var_list)
+        # build 関数はイテレーション毎に呼び出されるので, 初めの1度のみ処理されるようにする
         if hasattr(self, "_built") and self._built:
             return
         self._built = True
-        ###
-        
+
+        # 
         self._y = list()
-        # 更新前のパラメータを保持する変数
-        self._tmp_variable = list()
-        for var in var_list:
-            # 初期値は0で設定される
+        for variable in var_list:
             self._y.append(
                 self.add_variable_from_reference(
-                    model_variable = var, variable_name = "y"
-                )
-            )
-            self._tmp_variable.append(
-                self.add_variable_from_reference(
-                    model_variable = var, variable_name = "tmp_variable"
+                    model_variable = variable, variable_name = "y",
+                    # 初期値は0
+                    initial_value = tf.zeros(shape = variable.shape)
                 )
             )
     
     def update_step(self, gradient, variable):
-        var_key = self._var_key(variable)
-        y = self._y[self._index_dict[var_key]]
-        tmp_variable = self._tmp_variable[self._index_dict[var_key]]
+        # ハイパーパラメータはパラメータと同じデータ型にキャストする
+        learning_rate = tf.cast(self._learning_rate, variable.dtype)
+        alpha = tf.cast(self._alpha, variable.dtype)
+        beta = tf.cast(self._beta, variable.dtype)
 
-        # パラメータを保持
-        tmp_variable.assign(variable)
-        # zを更新
-        variable.assign(variable + self._learning_rate * ( (1 / self._beta - self._alpha) *  variable - 1 / self._beta * y - self._beta * gradient) )
-        y.assign(y + self._learning_rate * ( (1 / self._beta - self._alpha) *  tmp_variable - 1 / self._beta * y ) )
+        # 保持する変数を取得するためのキー
+        var_key = self._var_key(variable)
+
+        # yを取得
+        y = self._y[self._index_dict[var_key]]
+        
+        # 更新前のパラメータを保持
+        tmp_variable = variable.value()
+        # パラメータは assign 関数で更新する
+        variable.assign( variable + learning_rate * ( ((1/beta) - alpha) * variable - (1/beta) * y - beta * gradient ) )
+        # yを更新
+        y.assign( y + learning_rate * ( ((1/beta) - alpha) * tmp_variable - (1/beta) * y ) )
